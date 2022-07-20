9f7549bed354c1037b275a2c2ef755e8a0a52c17
안재영 <88701220+TheLeaderAhn@users.noreply.github.com>
2022-07-21 00:28:38 +0900

    [ch2] variable 공부

---
+++ ch2/src/main/java/com/javastudy/ch2/Variable.java
@@ -0,0 +1,25 @@
+package com.javastudy.ch2;
+
+public class Variable {
+
+    public static void main(String[] args) {
+
+        System.out.println("Variable Test:::::");
+
+        int age;
+        // initialization
+        int age2 = 25;
+        int a;
+        int b;
+        int x = 0;
+        int y = 0;
+
+        // 초기화란 변수를 사용하기전에 처음으로 값을 저장하는 것
+
+
+
+
+    }
+
+
+}
