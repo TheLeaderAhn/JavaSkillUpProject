2cb5faa171053459d1536f5d5abf04fcca588f4e
안재영 <88701220+TheLeaderAhn@users.noreply.github.com>
2022-07-21 00:01:32 +0900

    ch1 Hello world -javaC로 실행

---
+++ hello/src/main/java/com/example/hello/Hello.java
@@ -0,0 +1,19 @@
+package com.example.hello;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+
+    /*
+    * Date : 2022 07. 20
+    * source : Hello.java
+    * Name : 안재영
+    *
+    * */
+
+        System.out.println("Hello /*이것은 주석이 아님*/ world");
+        System.out.println("Hello, World // 이것도 주석이 아님");
+
+
+    }
+}
